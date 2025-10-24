201aafe832e2d18cd0e6d902f7810f29568cb51e
Robert Wan <wantunglok@gmail.com>
2025-10-25 09:44:27 +1100

    setup table view in settings tab

--- banknotes-reader/SettingsViewController.swift
+++ banknotes-reader/SettingsViewController.swift
@@ -7,23 +7,26 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-
+class SettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let settings = ["A", "B", "C", "D"]
+    
+    @IBOutlet weak var settingsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        settingsTableView.dataSource = self
+        settingsTableView.delegate = self
+        settingsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "SettingsTableViewCell")
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settings.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsTableViewCell", for: indexPath)
+        cell.textLabel?.text = settings[indexPath.row]
+        return cell
+    }
 }
